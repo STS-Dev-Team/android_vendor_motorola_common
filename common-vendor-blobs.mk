02dd3a07efa1cfff9282986ada6bde2947816421
dhacker29 <davidhackerdvm@gmail.com>
2012-08-27 22:02:41 -0500

    move wifi tether backup squisher to vendor common

---
+++ common-vendor-blobs.mk
@@ -0,0 +1,26 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+VENDOR_FOLDER := vendor/motorola/common
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    $(VENDOR_FOLDER)/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
+    $(VENDOR_FOLDER)/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
+    $(VENDOR_FOLDER)/prebuilt/bin/50-backup.sh:system/addon.d/50-backup.sh
+
+# WirelessTether
+PRODUCT_PACKAGES += wifi_tether_v3_2-beta1
+PRODUCT_COPY_FILES += \
+    $(VENDOR_FOLDER)/prebuilt/lib/libwtnativetask.so:system/lib/libwtnativetask.so \
---
+++ common-vendor.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Pick up overlay for features that depend on non-open-source files
+#DEVICE_PACKAGE_OVERLAYS := vendor/motorola/common/overlay
+
+PRODUCT_PACKAGES := \
+    librs_jni \
+    VideoEditor \
+    VoiceDialer \
+    Basic \
+    HoloSpiralWallpaper \
+    MagicSmokeWallpapers \
+    NoiseField \
+    Galaxy4 \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    PhaseBeam \
+
+# Publish that we support the live wallpaper feature.
+PRODUCT_COPY_FILES := \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
+
+$(call inherit-product, vendor/motorola/common/common-vendor-blobs.mk)
+$(call inherit-product, vendor/motorola/common/prebuilt/Android.mk)
