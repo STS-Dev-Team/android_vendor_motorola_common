42e3f57622e26137ba957627abfbab796ac87d45
dhacker29 <davidhackerdvm@gmail.com>
2013-01-02 03:45:14 -0600

    Update drm props

--- common_drm_phone.mk
+++ common_drm_phone.mk
@@ -2,14 +2,11 @@ VENDOR_FOLDER := vendor/motorola/common
 
 # Blobs necessary for drm
 PRODUCT_COPY_FILES +=  \
-    $(VENDOR_FOLDER)/proprietary/common_drm/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
-    $(VENDOR_FOLDER)/proprietary/common_drm/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar \
-    $(VENDOR_FOLDER)/proprietary/common_drm/phone/lib/libfrsdk.so:system/lib/libfrsdk.so \
-    $(VENDOR_FOLDER)/proprietary/common_drm/phone/lib/libWVphoneAPI.so:system/lib/libWVphoneAPI.so \
-    $(VENDOR_FOLDER)/proprietary/common_drm/phone/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
-    $(VENDOR_FOLDER)/proprietary/common_drm/phone/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
-    $(VENDOR_FOLDER)/proprietary/common_drm/phone/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
-    $(VENDOR_FOLDER)/proprietary/common_drm/phone/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so
+    $(VENDOR_FOLDER)/proprietary/common_drm/lib/libWVphoneAPI.so:system/lib/libWVphoneAPI.so \
+    $(VENDOR_FOLDER)/proprietary/common_drm/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
+    $(VENDOR_FOLDER)/proprietary/common_drm/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+    $(VENDOR_FOLDER)/proprietary/common_drm/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
+    $(VENDOR_FOLDER)/proprietary/common_drm/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so
 
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
