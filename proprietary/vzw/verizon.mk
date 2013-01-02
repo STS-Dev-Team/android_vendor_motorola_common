4d41fa1d329202b3319ecb6def0d0ac368a8b45b
dhacker29 <davidhackerdvm@gmail.com>
2013-01-02 09:16:07 -0600

    derp

--- proprietary/vzw/verizon.mk
+++ proprietary/vzw/verizon.mk
@@ -25,7 +25,7 @@ PRODUCT_PACKAGES += \
 
 #Permissions and Lib for Verizon Wireless Apps
 PRODUCT_COPY_FILES += \
-    vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
-    vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.hardware.lte.xml:system/etc/permissions/com.vzw.hardware.lte.xml \
-    vendor/aokp/prebuilt/vzw/etc/permissions/com.vzw.vzwapnlib.xml:system/etc/permissions/com.vzw.vzwapnlib.xml \
-    vendor/aokp/prebuilt/vzw/lib/libmotricity.so:system/lib/libmotricity.so
+    vendor/motorola/common/proprietary/vzw/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
+    vendor/motorola/common/proprietary/vzw/com.vzw.hardware.lte.xml:system/etc/permissions/com.vzw.hardware.lte.xml \
+    vendor/motorola/common/proprietary/vzw/com.vzw.vzwapnlib.xml:system/etc/permissions/com.vzw.vzwapnlib.xml \
+    vendor/motorola/common/proprietary/vzw/libmotricity.so:system/lib/libmotricity.so
